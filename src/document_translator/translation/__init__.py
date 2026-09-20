ae8cf2f0c8dbb2cb390937654a944e3be98ce684
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-20 10:53:47 +0530

    phase 5: export document translation orchestration

--- src/document_translator/translation/__init__.py
+++ src/document_translator/translation/__init__.py
@@ -1,1 +1,5 @@
-"""Translation providers and contracts."""
+"""Translation providers and orchestration."""
+
+from document_translator.translation.document import translate_document
+
+__all__ = ["translate_document"]

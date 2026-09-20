cd1eb0695507b992931774b9b65398d26453f1e0
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-20 11:00:59 +0530

    fix(web): organize validation imports

--- src/document_translator/web/app.py
+++ src/document_translator/web/app.py
@@ -18,8 +18,7 @@ from document_translator.models import TARGET_LANGUAGES, Language
 from document_translator.translation.document import translate_document
 from document_translator.translation.google_cloud import GoogleCloudTranslationProvider
 from document_translator.translation.service import TranslationService
-from document_translator.validation.validator import render_document_report
-from document_translator.validation.validator import validate_document
+from document_translator.validation.validator import (\n    render_document_report,\n    validate_document,\n)
 
 _MAX_UPLOAD_BYTES = 10 * 1024 * 1024
 _ALLOWED_SUFFIX = ".docx"

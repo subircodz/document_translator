7b112c406ca25a934daf71afa4910eb92c89a5d9
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-20 11:00:31 +0530

    fix(web): remove unused validation import

--- src/document_translator/web/app.py
+++ src/document_translator/web/app.py
@@ -18,7 +18,6 @@ from document_translator.models import TARGET_LANGUAGES, Language
 from document_translator.translation.document import translate_document
 from document_translator.translation.google_cloud import GoogleCloudTranslationProvider
 from document_translator.translation.service import TranslationService
-from document_translator.validation.report import ValidationStatus
 from document_translator.validation.validator import render_document_report
 from document_translator.validation.validator import validate_document
 

515a549d9c3ef64eb1be2fd1d0fe944e81ded2bd
Subir Sutradhar <ssutradhar35@gmail.com>
2026-09-20 10:42:43 +0530

    protect numeric tokens as well

--- src/document_translator/protection/tokens.py
+++ src/document_translator/protection/tokens.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 _TOKEN_PATTERN = re.compile(
     r"(?:https?://\S+|www\.\S+|[\w.+-]+@[\w.-]+\.\w+|"
-    r"\b(?:ORD|ID|REF)[-_][A-Z0-9-]+\b)"
+    r"\b(?:ORD|ID|REF)[-_][A-Z0-9-]+\b|(?<![\w])\d+(?:[.,]\d+)*(?:%)?)"
 )
 
 
